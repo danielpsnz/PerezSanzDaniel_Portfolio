e62ada86aa8455beb5fbdfadb50329be1352e7e7
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-06 19:17:27 +0000

    added particlejs and the first part of home section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,6 +9,9 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
