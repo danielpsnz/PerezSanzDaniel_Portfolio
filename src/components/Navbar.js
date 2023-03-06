4c0430108df3ed55e3542bc9834c946700bfa7ae
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-06 00:13:39 +0000

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <Navbar
+            fixed="top"
+            className="navbar"
+        >
+            <Container>
+                <Navbar.Brand href="/" className="d-flex">
+                    <h1>Daniel Pérez</h1>
+                </Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls="responsive-navbar-nav"
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto" defaultActiveKey="#home">
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/">
+                                Home
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/">
+                                About
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/">
+                                Skills
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/">
+                                Projects
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/">
+                                Contact me
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Button
+                                href=""
+                                target="_blank"
+                                className="fork-btn-inner"
+                            >
+                                Resume
+                            </Button>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
