9f20973d715a0d9f08b65914f60b2b55b3dc2dd4
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-30 18:59:07 +0200

    fixed footer issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import Project from "./components/Project";
